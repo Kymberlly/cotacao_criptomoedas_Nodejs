a8d330b994d4d29f7f671f1641597ed2c611898a
Kymberlly Glaucia de Melo <kymberlly.g.melo@avanade.com>
2021-09-21 15:03:06 -0300

    Criação de Swagger para API - V. 1.0.0

--- config/express.js
+++ config/express.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const consign = require('consign');
 
 module.exports = () => {
+
+    const optionsSwagger = {
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "API Cotação de Criptomoedas",
+                version: "1.0.0",
+                description: "Cotação de criptomoeda utilizando Nodejs e Mysql.",
+                contact: {
+                    name: "Kymberlly Melo",
+                    url: "https://github.com/Kymberlly"
+                },
+                servers: [
+                    { url: "http://localhost:3000" }
+                ]
+            }
+        },
+        apis: ['./controllers/monitoramento.js']
+    };
+
+    const specs = swaggerJsDoc(optionsSwagger);
     const app = express();
 
     app.use(express.json());
+    app.use("/", swaggerUi.serve, swaggerUi.setup(specs));
+
     consign().include('./controllers').into(app);
 
     return app;
--- controllers/monitoramento.js
+++ controllers/monitoramento.js
@@ -1,3 +1,61 @@
+/**
+* @swagger
+*   components:
+*       schemas:
+*           Cotacao:
+*               type: object
+*               required:
+*                   - periodo
+*                   - moeda
+*               properties:
+*                   periodo:
+*                       type: integer
+*                       description: Período da cotação valores entre 5, 10 e 15
+*                   moeda:
+*                       type: list
+*                       description: Sigla da moeda a ser cotada
+*               example:
+*                   periodo: 1
+*                   moeda: ["BTC_XMR", "BTC_KNC", "BTC_ZEC"]
+ */
+
+/**
+ * @swagger
+ * /monitorar/{periodo}/{moeda}:
+ *  get:
+ *      sumary: Armazena no banco de dados o valor da cotação gerada pela moeda
+ *      parameters:
+ *          - in: path
+ *            name: periodo
+ *            schema:
+ *                type: integer
+ *            required: true
+ *            description: O período da cotação
+ *          - in: path
+ *            name: moeda
+ *            schema:
+ *                type: string
+ *            required: true
+ *            description: A sigla do par de moedas
+ *      responses:
+ *          "200":
+ *              description: Sucesso ao armazenar a cotação
+ * 
+ * /monitoramento:
+ *  post:
+ *      sumary: Armazena no banco de dados o valor da cotação gerada pela moeda
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Cotacao'
+ *      responses:
+ *          "200":
+ *              description: Sucesso ao armazenar a cotação
+ *                  
+ */
+
 const Monitoramento = require('../models/monitoramento.js');
 
 module.exports = app => {
@@ -7,8 +65,8 @@ module.exports = app => {
         const periodoObservacao = parseInt(periodo);
 
         Monitoramento.observador(periodoObservacao, moeda)
-        .then(resultado => res.json(resultado))
-        .catch(erro => res.status(400).json({erro}));
+            .then(resultado => res.json(resultado))
+            .catch(erro => res.status(400).json({ erro }));
     });
 
 };
